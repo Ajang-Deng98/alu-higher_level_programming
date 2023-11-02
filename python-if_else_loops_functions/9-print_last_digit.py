157f9772f49a10d818958813448044a9ac5b81c8
Ajang-Deng98 <a.deng3@alustudent.com>
2023-11-02 13:48:50 +0200

    ajang

--- python-if_else_loops_functions/9-print_last_digit.py
+++ python-if_else_loops_functions/9-print_last_digit.py
@@ -1,8 +1,8 @@
-
 #!/usr/bin/env python3
-print_last_digit = _import_('9-print_last_digit').print_last_digit
-
-print_last_digit(98)
-print_last_digit(0)
-r = print_last_digit(-1024)
-print(r)
+def print_last_digit(number):
+    if number < 0:
+        last_num = (-number % 10)
+    elif number >= 0:
+        last_num = number % 10
+    print("{:d}".format(last_num), end="")
+    return last_num
