866addbbf1509fdb14945e08d17aa6034bea85d9
Ajang-Deng98 <a.deng3@alustudent.com>
2023-11-30 05:09:33 -0800

    7-insert_value.sql

--- SQL_introduction/7-insert_value.sql
+++ SQL_introduction/7-insert_value.sql
@@ -1,2 +1,2 @@
---  script that inserts a new row in the table first_table.
-INSERT INTO "3first_table" ("id"," name") VALUES (89, "Best School");
+-- Inserts a new row into the table first_table.
+INSERT INTO `first_table` (`id`, `name`) VALUES (89, "Best School");
