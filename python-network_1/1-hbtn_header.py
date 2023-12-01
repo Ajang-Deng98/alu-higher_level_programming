cb3d0587598e19fb9a977aaddcbdbf5e05441704
Ajang-Deng98 <a.deng3@alustudent.com>
2023-12-01 17:55:57 +0000

    1-hbtn_header.py

--- python-network_1/1-hbtn_header.py
+++ python-network_1/1-hbtn_header.py
@@ -1,4 +1,5 @@
-cript that takes in a URL,
+#!/usr/bin/python3
+"""A script that takes in a URL,
 Sends a request to the URL,
 And displays the value of the X-Request-Id
  variable found in the header ofthe response.
@@ -6,7 +7,6 @@ And displays the value of the X-Request-Id
 
 import sys
 import urllib.request
-
 
 
 if __name__ == "__main__":
